c0f52d436541cd89e80eb2254053c27a06f236b7
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-07-20 22:45:51 +0800

    Update Launch.java

--- src/Main/Launch.java
+++ src/Main/Launch.java
@@ -1,1 +1,26 @@
+package Main;
 
+public class Launch {
+	
+	private final String title="Sudoku Solver";
+	
+	private String path="/Users/tengdajiang/Desktop/test.txt";
+	
+	private final int width=720;
+	
+	private final int height=720;
+	
+	public Window window;
+	
+	private Display display;
+	
+	public static void main(String[]args) {
+		new Launch();
+	}
+	
+	public Launch() {
+		window=new Window(title,width,height);
+		display=new Display(window,path);
+		display.start();
+	}
+}
