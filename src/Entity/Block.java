b21062c502b6d05acebf54eaced3d215a6e8f607
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-26 21:32:11 -0700

    Update Block.java

--- src/Entity/Block.java
+++ src/Entity/Block.java
@@ -1,1 +1,56 @@
+package Entity;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import Interface.Renderable;
+import Interface.Tickable;
+import Main.Display;
+
+public class Block extends Entity implements Tickable,Renderable {
+	
+	private int value;
+	
+	public Block(Display display,int x,int y,int value) {
+		super(display,x,y);
+		this.value=value;
+	}
+
+	public void tick() {
+		if(display.getKeyActionHandler().getRemove()) {
+			if(display.getMouseActionHandler().getPressed()) {
+				int x=display.getMouseMotionHandler().getX();
+				int y=display.getMouseMotionHandler().getY();
+				
+				if(x>this.x && x<this.x+display.getResolution()) {
+					if(y>this.y && y<this.y+display.getResolution()) {
+						value=0;
+					}
+				}
+			}
+		}
+	}
+	
+	public void render(Graphics2D g) {
+		if(value!=0) {
+			g.setColor(Color.DARK_GRAY);
+			g.fillRect(x,y,display.getResolution(),display.getResolution());
+			
+			Font font=new Font("TimesRoman",Font.BOLD,30);
+			
+			Rectangle rectangle=new Rectangle(x,y,display.getResolution(),display.getResolution());
+			
+			this.drawCenterText(g,font,String.valueOf(value),rectangle);
+		}
+	}
+	
+	public void setValue(int value) {
+		this.value=value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
