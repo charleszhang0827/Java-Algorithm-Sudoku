06aa3defd8ef23d6b375522a7b8b606abad5583a
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-26 21:36:11 -0700

    Update State.java

--- src/State/State.java
+++ src/State/State.java
@@ -1,1 +1,23 @@
+package State;
 
+import Interface.Renderable;
+import Interface.Tickable;
+import Main.Display;
+
+public abstract class State implements Tickable,Renderable {
+	public static State currentState;
+	
+	public static void setState(State state) {
+		currentState=state;
+	}
+	
+	public static State getState() {
+		return currentState;
+	}
+	
+	protected Display display;
+	
+	public State(Display display) {
+		this.display=display;
+	}
+}
