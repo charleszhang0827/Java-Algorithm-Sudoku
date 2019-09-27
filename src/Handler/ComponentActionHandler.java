07f4580c8def74bae7bd4094eff1274d2df25b7d
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-26 21:29:47 -0700

    Update ComponentActionHandler.java

--- src/Handler/ComponentActionHandler.java
+++ src/Handler/ComponentActionHandler.java
@@ -1,1 +1,31 @@
+package Handler;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+
+import Main.Display;
+
+public class ComponentActionHandler implements ActionListener {
+
+	private Display display;
+	
+	public ComponentActionHandler(Display display) {
+		this.display=display;
+	}
+	
+	public void actionPerformed(ActionEvent actionEvent) {
+		JTextField textField=display.getWindow().getTextField();
+		
+		if(textField.getText().equals("make")) {
+			display.getRunState().setDFSMaking(true);
+			textField.setText("");
+		}else if(textField.getText().equals("solve")) {
+			display.getRunState().setDFSolving(true);
+			textField.setText("");
+		}else if(textField.getText().equals("reset")) {
+			display.getRunState().reset();
+			textField.setText("");
+		}
+	}
+}
