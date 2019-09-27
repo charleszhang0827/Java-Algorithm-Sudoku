57206fdbdfea216e1b375b7ab4b4969e6eca50e3
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-26 21:33:32 -0700

    Update Runner.java

--- src/Runner/Runner.java
+++ src/Runner/Runner.java
@@ -1,1 +1,16 @@
+package Runner;
 
+import Main.Display;
+
+public abstract class Runner implements Runnable {
+	
+	protected Display display;
+	
+	public Runner(Display display) {
+		this.display=display;
+	}
+	
+	public abstract void start();
+	
+	public abstract void stop();
+}
