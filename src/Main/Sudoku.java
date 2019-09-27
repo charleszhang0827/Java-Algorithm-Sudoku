0cdd5f5346ef09dce11372b8adfeb026abc1e8f8
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2019-09-26 21:25:31 -0700

    Update Sudoku.java

--- src/Main/Sudoku.java
+++ src/Main/Sudoku.java
@@ -1,1 +1,23 @@
+package Main;
 
+public class Sudoku {
+	private final String title="Sudoku";
+	
+	private final int width=810;
+	
+	private final int height=810;
+	
+	public Window window;
+	
+	private Display display;
+	
+	public static void main(String[]args) {
+		new Sudoku();
+	}
+	
+	private Sudoku() {
+		window=new Window(title,width,height);
+		display=new Display(window);
+		display.start();
+	}
+}
